ebe7e0eee50769ebeaf6f44c118d3c3474e016c4
inspiringz <z@3nd.xyz>
2021-04-06 19:33:57 +0800

    update

--- _includes/pageview-providers/leancloud/leancloud.js
+++ _includes/pageview-providers/leancloud/leancloud.js
@@ -13,7 +13,7 @@
     appKey = options.appKey;
     appClass = options.appClass;
     AV.init({
-      serverURLs: 'https://hrvndkhv.lc-cn-n1-shared.com',
+      serverURLs: 'https://avoscloud.com/',
       appId: appId,
       appKey: appKey
     });
